0a6eda34c0db91811c73648cbf30c363285eba76
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-02-05 17:06:08 +0600

    13 - Implementing the height and width controls

--- src/curvy/edit.js
+++ src/curvy/edit.js
@@ -1,13 +1,13 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import './editor.scss';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import { PanelBody, ToggleControl, RangeControl } from '@wordpress/components';
 import metadata from './block.json';
 import { Curve } from './components/Curve';
 
 
 export default function Edit({ attributes, setAttributes }) {
-	const { enableTopCurve } = attributes;
+	const { enableTopCurve, topWidth, topHeight } = attributes;
 	const {className, ...blockProps} = useBlockProps();
 
 	console.log( className );
@@ -29,6 +29,26 @@ export default function Edit({ attributes, setAttributes }) {
 					/>
 					<span>{ __("Enable Top Curve", metadata.textdomain) }</span>
 				</div>
+
+				<RangeControl 
+				label={__("Width", metadata.textdomain)} 
+				min={100}
+				max={300}
+				value={ topWidth || 100 }
+				onChange={(newValue)=>{
+					setAttributes({topWidth: parseInt(newValue)})
+				}}
+				/>
+
+				<RangeControl 
+				label={__("Height", metadata.textdomain)} 
+				min={0}
+				max={200}
+				value={ topHeight }
+				onChange={(newValue)=>{
+					setAttributes({topHeight: parseInt(newValue)})
+				}}
+				/>
 			</PanelBody>
 		</InspectorControls>
 		</>
