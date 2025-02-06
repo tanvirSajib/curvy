8a265e1d9d0e969d242cc9d4647589c7143eb37c
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-02-06 11:39:24 +0600

    (d/a) 21 - Create dynamic front end view for curvy block

---
+++ src/curvy/render.php
@@ -0,0 +1,7 @@
+<?php 
+$block_wrapper_attributes = get_block_wrapper_attributes();
+
+// wp_send_json($block_wrapper_attributes); 
+?>
+
+<div <?php echo $block_wrapper_attributes; ?>>This  is test</div>
