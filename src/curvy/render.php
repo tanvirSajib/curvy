101c5e441b36cd61b26ffea20436bc7cf7b319c6
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-02-06 11:53:44 +0600

    (d/a) 22 - Add extra styles and inner blocks to front end

--- src/curvy/render.php
+++ src/curvy/render.php
@@ -1,7 +1,11 @@
 <?php 
-$block_wrapper_attributes = get_block_wrapper_attributes();
+$block_wrapper_attributes = get_block_wrapper_attributes([
+    'class'     => 'alignfull'
+]);
 
-// wp_send_json($block_wrapper_attributes); 
+// wp_send_json($content); 
 ?>
 
-<div <?php echo $block_wrapper_attributes; ?>>This  is test</div>
+<div <?php echo $block_wrapper_attributes; ?>>
+    <?php echo $content; ?>
+</div>
